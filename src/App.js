df92486c885ba956a337d34b112b9c22d8cf79a5
Roberto Morales <roberto@morales.com.co>
2022-02-24 20:07:46 -0500

    Componente Variables terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Parrafo from './components/Parrafo';
+import Variables from './components/Variables';
 
 function App() {
   return (
     <div className="App">
      <Parrafo/>
+     <Variables/>
     </div>
   );
 }
---
+++ src/components/Variables.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Variables = () => {
+    const saludo = 'Hola mundo desde una constante'
+    const imagen = 'https://picsum.photos/500'
+    const texto_imagen = 'Imagen aleatoria'
+
+  return (
+    <div>
+        <h2>Nuevo Componente {saludo}</h2>
+        <img src={imagen} alt={texto_imagen}/>
+    </div>
+  )
+}
+
+export default Variables
