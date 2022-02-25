7f64f7c858bce493730012b7994c3fab33497b41
Roberto Morales <roberto@morales.com.co>
2022-02-24 19:57:43 -0500

    Componente parrafo creado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Parrafo from './components/Parrafo';
 
 function App() {
   return (
     <div className="App">
-      
+     <Parrafo/>
     </div>
   );
 }
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Fragment } from 'react'
+
+
+const Parrafo = () => {
+  return (
+    <div>
+        <p>Bienvenidos a mi sitio web</p>
+    </div>
+  )
+}
+
+export default Parrafo
