4bd4c06c57b6ac144678277fb6e4a395a89d8f47
Roberto Morales <roberto@morales.com.co>
2022-04-16 11:12:44 -0500

    interfaz crud básico

--- fundamentos/src/components/Formulario.jsx
+++ fundamentos/src/components/Formulario.jsx
@@ -31,34 +31,49 @@ const Formulario = () => {
 
   return (
     <div className='container mt-5'>
-        <h2>Fomulario</h2>
-        <form onSubmit ={guardarFrutas}>
-            <input 
-            className='form-control mb-2'
-            type = "text"
-            placeholder='Ingrese Fruta'
-            onChange={(e)=> setFruta(e.target.value)}
-            />
-            <input 
-            className='form-control mb-2'
-            placeholder='Ingrese Descripción'
-            type="text"
-            onChange={(e)=> setDescripcion(e.target.value)}
-            />
-            <button 
-            className='btn btn-primary btn-block'
-            type='submit'
-            >Agregar</button>
-        </form>
-        <br/>
-        {
-            listaFrutas.map((item,index)=>(
-                <div key={index}>
-                    <h4>{item.nombreFruta}-{item.nombreDescripcion}</h4>
-                </div>    
-            ))
-        }
-
+        <h1 className='text-center'>CRUD BÁSICO</h1>
+        <hr/>
+        <div className='row'>
+            <div className='col-8'>
+                <h4 className='text-center'>Listado de Frutas</h4>
+                <ul className='list-group'>
+                    {
+                        listaFrutas.map((item,index)=>(
+                            <li className='list-group-item' key={index}>
+                                <spam className='lead'>{item.nombreFruta}-{item.nombreDescripcion}</spam>
+                                <button className='btn btn-danger btn-sm float-end mx-2'>
+                                Eliminar
+                                </button>
+                                <button className='btn btn-warning btn-sm float-end'>
+                                Editar
+                                </button>
+                            </li>    
+                        ))
+                    }
+                </ul>
+            </div>
+            <div className='col-4'>
+                <h4 className='text-center'></h4>
+                <form onSubmit ={guardarFrutas}>
+                    <input 
+                    className='form-control mb-2'
+                    type = "text"
+                    placeholder='Ingrese Fruta'
+                    onChange={(e)=> setFruta(e.target.value)}
+                    />
+                    <input 
+                    className='form-control mb-2'
+                    placeholder='Ingrese Descripción'
+                    type="text"
+                    onChange={(e)=> setDescripcion(e.target.value)}
+                    />
+                    <button 
+                    className='btn btn-primary btn-block'
+                    type='submit'
+                    >Agregar</button>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
